a4fce259ea4d1f472b9686c7868e5a01d9a69508
washizawa83 <washizawa83@gmail.com>
2024-09-08 05:48:30 +0900

    ヘッダー作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,56 @@
+'use client'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+import { Header } from '@/app/_layouts/Header'
 import './globals.css'
+
+type ThemeContextType = {
+    theme: string
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode
 }>) {
+    const [theme, setTheme] = useState<'light' | 'dark'>('light')
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const savedTheme = localStorage.getItem('theme') as 'light' | 'dark'
+            setTheme(savedTheme || 'light')
+        }
+    }, [])
+
+    useEffect(() => {
+        if (theme) {
+            localStorage.setItem('theme', theme)
+        }
+    }, [theme])
+
+    const toggleTheme = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
+
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className={theme}>
+            <body>
+                <Header />
+                <ThemeContext.Provider value={{ theme, toggleTheme }}>
+                    {children}
+                </ThemeContext.Provider>
+            </body>
         </html>
     )
+}
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext)
+    if (!context) {
+        throw new Error('useTheme must be used within a ThemeProvider')
+    }
+    return context
 }
